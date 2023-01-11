decc6e230c00de7fbcdd148629543b0542cee83e
Andrei Borisov <o10i@ya.ru>
2023-01-11 20:55:24 +0300

    BookingControllerTest completed.

--- src/main/java/ru/practicum/shareit/request/dto/RequestShortDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/RequestShortDto.java
@@ -2,10 +2,12 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class RequestShortDto {
     @NotBlank
