e020d12204d2fabf935eb927044f581d77c56f69
Andrei Borisov <o10i@yandex.ru>
2023-01-24 13:09:29 +0300

    Refactor User tests.

--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.user;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.Email;
@@ -11,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
